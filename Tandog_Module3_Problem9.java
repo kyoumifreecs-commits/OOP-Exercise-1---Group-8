5f603ca785fbb832300b6cd422b22033feeb5567
Kyee <kyoumifreecs@gmail.com>
2026-09-16 16:43:45 +0800

    Update Tandog_Module3_Problem9.java

--- Tandog_Module3_Problem9.java
+++ Tandog_Module3_Problem9.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Tandog_Module2_Problem4;
+package Tandog_Module3_Problem9;
 
 /**
  *
