28ca6508fdc7ef347cc280b6331a539692df6247
Kyee <kyoumifreecs@gmail.com>
2026-09-16 16:49:01 +0800

    Update Tandog_Module3_Problem4.java

--- Tandog_Module3_Problem4.java
+++ Tandog_Module3_Problem4.java
@@ -1,26 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author HP
+ */
 import java.util.Scanner;
 
 public class Tandog_Module3_Problem4 {
     public static void main(String[] args) {
-      
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter weight in kilograms: ");
+        double weight = scanner.nextDouble();
 
-        System.out.print("Enter first integer: ");
-        int a = scanner.nextInt();
-
-        System.out.print("Enter second integer: ");
-        int b = scanner.nextInt();
-
-        int andResult = a & b;
-        int orResult = a | b;
-        int xorResult = a ^ b;
-        int notResult = ~a;
-        int leftShiftResult = a << 1;
-        int rightShiftResult = a >> 1;
-
-        System.out.println("AND=" + andResult + "; OR=" + orResult + 
-                           "; XOR=" + xorResult + "; NOT a=" + notResult + 
-                           "; a<<1=" + leftShiftResult + "; a>>1=" + rightShiftResult);
+        System.out.print("Enter height in meters: ");
+        double height = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.println("BMI: " + bmi);
 
         scanner.close();
     }
