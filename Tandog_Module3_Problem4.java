8eadccfb9ad039680e06d87304751481ca841d6b
Kyee <kyoumifreecs@gmail.com>
2026-09-16 16:43:07 +0800

    Update Tandog_Module3_Problem4.java

--- Tandog_Module3_Problem4.java
+++ Tandog_Module3_Problem4.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Tandog_Module2_Problem4;
+import java.util.Scanner;
 
-/**
- *
- * @author HP
- */
 public class Tandog_Module3_Problem4 {
-    
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int b = scanner.nextInt();
+
+        int andResult = a & b;
+        int orResult = a | b;
+        int xorResult = a ^ b;
+        int notResult = ~a;
+        int leftShiftResult = a << 1;
+        int rightShiftResult = a >> 1;
+
+        System.out.println("AND=" + andResult + "; OR=" + orResult + 
+                           "; XOR=" + xorResult + "; NOT a=" + notResult + 
+                           "; a<<1=" + leftShiftResult + "; a>>1=" + rightShiftResult);
+
+        scanner.close();
+    }
 }
